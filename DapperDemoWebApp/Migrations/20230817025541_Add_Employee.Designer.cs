4f0251f3d0d537912e91a56cd003068892f9f111
RedAngelRedux <sammy@sammynava.com>
2023-08-16 20:00:13 -0700

    Add Employee Model with Foreign Key to Company and Fluent API instead of using propery Attributes.

--- DapperDemoWebApp/Migrations/20230817025541_Add_Employee.Designer.cs
+++ DapperDemoWebApp/Migrations/20230817025541_Add_Employee.Designer.cs
@@ -3,6 +3,7 @@ using DapperDemoWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DapperDemoWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230817025541_Add_Employee")]
+    partial class Add_Employee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
